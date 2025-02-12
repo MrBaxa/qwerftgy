69995c011f8a5425a2386ff4f3088373e5d186b6
MrBaxa <maripovbahti@gmail.com>
2025-02-12 18:41:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = prompt("Введите ваше имя:");
+let spaceId = prompt("Введите Space ID:");
+let spacePassword = prompt("Введите Space Password:");
+
+let userInfo = {
+    name: name,
+    ID: spaceId,
+    password: spacePassword
+};
+console.log(userInfo);
